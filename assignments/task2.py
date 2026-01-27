6470cb8790b32e0dd1e6c387d008c8b6242d31f1
kalki <kalki596367@gmail.com>
2026-01-28 00:28:38 +0545

    updated

--- assignments/task2.py
+++ assignments/task2.py
@@ -1,9 +1,16 @@
 #wap to find the factorial of first n numbers( using for loops)
 
 n=int(input("enter nth numbers:"))
-sum=0
+# sum=1
+# # range(1, 5)   # gives 1, 2, 3, 4 (NOT 5)
+# # range(1, 6)   # gives 1, 2, 3, 4, 5 (NOT 6)
+# for i in range(1,n + 1):
+#     sum=sum * i
+    
+# print(sum)    
 
-for i in range(n + 1):
-    sum=sum +i
-    i+=1
-print(sum)    
+
+# easy way to find factorial using math module 
+import math
+result = math.factorial(n)
+print(result)
